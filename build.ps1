ef118040594efd4ece316c8232a554ef06921bf0
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2017-04-11 03:05:30 +1000

    Simplify build file

--- build.ps1
+++ build.ps1
@@ -2,15 +2,13 @@
 $ErrorActionPreference = "Stop"
 if (-not $env:Configuration) { $env:Configuration = "Release" }
 if (-not $env:BuildRunner) { $env:PackageVersion = (gitversion | ConvertFrom-Json).NuGetVersionV2 }
+
+# Helper functions
 function Invoke-BuildStep { param([scriptblock]$cmd) & $cmd; if ($LASTEXITCODE -ne 0) { exit 1 } }
 
 # Build
-Push-Location src/GraphQL.BatchResolver/
-try {
-    Invoke-BuildStep { dotnet restore }
-    Invoke-BuildStep { dotnet build }
-    Invoke-BuildStep { dotnet pack --include-symbols --no-build }
-} finally { Pop-Location }
+Invoke-BuildStep { dotnet restore src/GraphQL.BatchResolver/GraphQL.BatchResolver.csproj }
+Invoke-BuildStep { dotnet pack src/GraphQL.BatchResolver/GraphQL.BatchResolver.csproj --include-symbols }
 
 # Done
 exit
